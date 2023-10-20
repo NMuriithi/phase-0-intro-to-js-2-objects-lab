6184056bba0fb240eb8fef241722af35c86a7079
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-20 10:12:10 -0700

    Complete assignmet-revisit lesson.

--- index.js
+++ index.js
@@ -32,3 +32,38 @@ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     return employee;
   }
     
+/* function createEmployee(name, streetAddress) {
+    return {
+      name: name,
+      streetAddress: streetAddress
+    };
+  }
+  let employee = createEmployee(John Doe, 123 Main St);
+console.log(employee);
+   */
+let employee = {
+    name: John Doe,
+    streetAddress: 123 Main St
+  };
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+}
+let updatedEmployee =updateEmployeeWithKeyAndValue(employee, key, value);
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+    
+
