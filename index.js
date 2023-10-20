dd1b7749ace7968937c2de26949253df1f86a186
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-20 10:28:28 -0700

    Complete assignmet-revisit lesson.

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ let employee = {
     streetAddress: "123 Main St"
   };
   function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return {
-      ...employee,
-      [key]: value
-    };
-}
-let updatedEmployee =updateEmployeeWithKeyAndValue(employee, key, value);
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+};
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
@@ -31,39 +30,6 @@ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     delete employee[key];
     return employee;
   }
-    
-/* function createEmployee(name, streetAddress) {
-    return {
-      name: name,
-      streetAddress: streetAddress
-    };
-  }
-  let employee = createEmployee(John Doe, 123 Main St);
-console.log(employee);
-   */
-let employee = {
-    name: John Doe,
-    streetAddress: 123 Main St
-  };
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return {
-      ...employee,
-      [key]: value
-    };
-}
-let updatedEmployee =updateEmployeeWithKeyAndValue(employee, key, value);
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-  }
-  function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = { ...employee };
-    delete newEmployee[key];
-    return newEmployee;
-  }
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
-  }
-    
+
+
 
