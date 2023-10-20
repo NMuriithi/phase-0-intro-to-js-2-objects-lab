4c0c9320309554d4575135f803b81d9b10c1d822
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-20 07:42:10 -0700

    Complete assignmet-revisit lesson.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+/* function createEmployee(name, streetAddress) {
+    return {
+      name: name,
+      streetAddress: streetAddress
+    };
+  }
+  let employee = createEmployee("John Doe", "123 Main St");
+console.log(employee);
+   */
+let employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+  };
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+}
+let updatedEmployee =updateEmployeeWithKeyAndValue(employee, "phone", "555-555-5555");
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+    
